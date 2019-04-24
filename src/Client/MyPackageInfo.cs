62a28b2a487c8a2b7697302689472857b9a4e608
Tangming <infly123@sina.com>
2019-04-24 08:34:43 +0800

    update

---
+++ src/Client/MyPackageInfo.cs
@@ -0,0 +1,17 @@
+﻿using SuperSocket.ProtoBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class MyPackageInfo : IPackageInfo
+    {
+        public MyPackageInfo(byte[] header,byte[] body)
+        {
+
+        }
+    }
+}
---
+++ src/Client/MyReceiveFilter.cs
@@ -0,0 +1,36 @@
+﻿using SuperSocket.ProtoBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class MyReceiveFilter : FixedHeaderReceiveFilter<MyPackageInfo>
+    {
+        public MyReceiveFilter() : base(4)
+        {
+
+        }
+
+        public override MyPackageInfo ResolvePackage(IBufferStream bufferStream)
+        {
+            byte[] header = bufferStream.Buffers[0].ToArray();
+            byte[] bodyBuffer = bufferStream.Buffers[1].ToArray();
+            byte[] allBuffer = bufferStream.Buffers[0].Array.CloneRange(0, (int)bufferStream.Length);
+            if (allBuffer.Length < 6) return null;
+            var isReply = allBuffer.Length == 6;
+            var package = new MyPackageInfo(bodyBuffer, bodyBuffer);
+            return package;
+        }
+
+        protected override int GetBodyLengthFromHeader(IBufferStream bufferStream, int length)
+        {
+            ArraySegment<byte> buffers = bufferStream.Buffers[0];
+            byte[] array = buffers.ToArray();
+            int bodyLength = array[length - 2] * 256 + array[length - 1];
+            return bodyLength;
+        }
+    }
+}
